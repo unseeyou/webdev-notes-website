f0400c2a87d94ee7e68d43bcb06838008419ad19
unseeyou <unseeyoubestmod@gmail.com>
2024-12-03 11:27:39 +1100

    feat: hide scroll to top button until page is scrolled a certain amount

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -10,3 +10,12 @@ function scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+
+window.onscroll = function() {
+    var scrollButton = document.getElementById("scrollButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
