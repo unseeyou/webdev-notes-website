a2664b94d194b1556ec5c4d754326c8adbabc49c
unseeyou <unseeyoubestmod@gmail.com>
2025-02-10 11:42:09 +1100

    docs: edit comment grammar

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -13,7 +13,7 @@ function scrollToTop() {
 
 window.onscroll = function() {
     var scrollButton = document.getElementById("scrollButton");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {  // use OR so that it works on both Chrome, Firefox, and Safari
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {  // use OR so that it works on Chrome, Firefox, and Safari
         scrollButton.style.display = "block";
     } else {
         scrollButton.style.display = "none";
