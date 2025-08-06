a2ac1dd5c8aae24d0ec069eed7be2731a3638361
unseeyou <unseeyoubestmod@gmail.com>
2025-08-06 15:15:44 +1000

    feat: fix waitress wrong host

--- api/index.py
+++ api/index.py
@@ -3,5 +3,5 @@ from waitress import serve
 
 if __name__ == "__main__":
     print("Starting up...")
-    serve(app, host='0.0.0.0', port=6970)
+    serve(app, host='localhost', port=6970)
     print("Server Closed")
